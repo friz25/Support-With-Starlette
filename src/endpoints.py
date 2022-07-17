eed024dadb1f8bace385b6f8c81323ad2a4e1742
frizmob@gmail.com <friz48625>
2022-07-17 15:15:22 +0300

    реализовали через Классы

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,6 +1,11 @@
+from starlette.endpoints import HTTPEndpoint
 from starlette.responses import PlainTextResponse
 from starlette.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
+
+from config.database import database
+from .models import support
+
 template = Jinja2Templates(directory='templates')
 
 # async def homepage(request):
@@ -9,9 +14,32 @@ template = Jinja2Templates(directory='templates')
 async def homepage(request):
     if request.method == 'POST':
         # print('AAAAAAAA')
-
+        form = await request.form()
+        query = support.insert().values(
+            username=form['username'],
+            email=form['email'],
+            message=form['message']
+        )
+        await database.execute(query)
         return RedirectResponse(request.url_for('get_support'), status_code=303)
     return template.TemplateResponse('index.html', {"request": request})
 
 async def get_support(request):
-    return template.TemplateResponse('support.html', {"request": request})
+    query = support.select()
+    results = await database.fetch_all(query)
+    return template.TemplateResponse('support.html', {"request": request, "results": results})
+
+class Home(HTTPEndpoint):
+    """ Реализуем через классы """
+    async def get(self, request):
+        return template.TemplateResponse('index.html', {"request": request})
+
+    async def post(self, request):
+        form = await request.form()
+        query = support.insert().values(
+            username=form['username'],
+            email=form['email'],
+            message=form['message']
+        )
+        await database.execute(query)
+        return RedirectResponse(request.url_for('get_support'), status_code=303)
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,8 @@
 from starlette.routing import Route
-from src.endpoints import homepage, get_support
+from src.endpoints import homepage, get_support, Home
 
 routes = [
     Route('/', homepage, methods=['GET', 'POST']),
     Route('/support', get_support, name='get_support'),
+    Route('/homepage', Home),
 ]
